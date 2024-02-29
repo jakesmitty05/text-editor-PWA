d122284c660532714a94a94dfb5a301641aa2723
Jakesmitty05 <jakesmitty05@gmail.com>
2024-02-29 02:35:38 -0500

    added code to get from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,7 +11,7 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-  
+
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('Saving to the database');
@@ -29,6 +29,20 @@ export const putDb = async (content) => {
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Getting from the database');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  
+  console.log('Result from the database', result);
+  return result;
+};
+
 
 initdb();
