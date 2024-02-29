31bb60cdfe4163b4a607a4edb15238942167172a
Jakesmitty05 <jakesmitty05@gmail.com>
2024-02-29 02:31:19 -0500

    added logic to add to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,9 +11,22 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-
+  
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Saving to the database');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+
+  const request = store.add({ content: content });
+
+  const result = await request;
+
+  console.log('Saved to the database', result);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
