130a3c62164904dafac0b9262a02d0a47182c4c4
Jakesmitty05 <jakesmitty05@gmail.com>
2024-03-10 21:21:26 -0400

    fixed bugs and errors in src-sw.js and webpack config and updated main package.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('Saving to the database');
 
@@ -20,14 +20,14 @@ export const putDb = async (content) => {
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
 
-  const request = store.add({ content: content });
+  const request = store.put({ id: 1, value: content });
 
   const result = await request;
 
   console.log('Saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// gets all the content from the database
 export const getDb = async () => {
   console.log('Getting from the database');
 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const WorkboxPlugin = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => {
   return {
@@ -21,13 +21,15 @@ module.exports = () => {
       
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Text Editor'
+        title: 'Webpack Plugin'
       }),
 
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
+
+      new MiniCssExtractPlugin(),
 
       new WebpackPwaManifest({
         fingerprints: false,
